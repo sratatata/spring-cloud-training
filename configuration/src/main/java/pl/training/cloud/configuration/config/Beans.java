46a1ebbf5a91476633b49b56daa44e3363652fe4
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-13 19:18:50 +0200

    Simple microservice

--- configuration/src/main/java/pl/training/cloud/configuration/config/Beans.java
+++
@@ -1,9 +0,0 @@
-package pl.training.cloud.configuration.config;
-
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
-
-@EnableConfigServer
-@Configuration
-public class Beans {
-}
--- discovery/src/main/java/pl/training/cloud/discovery/config/Beans.java
+++
@@ -1,9 +0,0 @@
-package pl.training.cloud.discovery.config;
-
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-
-@EnableEurekaServer
-@Configuration
-public class Beans {
-}
