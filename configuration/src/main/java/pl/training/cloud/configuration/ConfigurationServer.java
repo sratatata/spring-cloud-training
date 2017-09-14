4e6e3dc25f349f4b0e5f4750e8fd2fcedb5a0638
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-14 10:57:35 +0200

    Sleuth-and-Zipkin

--- configuration/src/main/java/pl/training/cloud/configuration/ConfigurationServer.java
+++ configuration/src/main/java/pl/training/cloud/configuration/ConfigurationServer.java
@@ -4,7 +4,8 @@ package pl.training.cloud.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//export ENCRYPT_KEY=SYMMETRICKEY
+// copy java jce jars to jre/lib/security
+// export ENCRYPT_KEY=SYMMETRICKEY
 @SpringBootApplication
 public class ConfigurationServer {
 
