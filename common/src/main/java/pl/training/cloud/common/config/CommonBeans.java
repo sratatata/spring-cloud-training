e74f7c2f2bf8515d4139c1911f11d4265a550875
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-14 14:35:42 +0200

    Security

--- common/src/main/java/pl/training/cloud/common/config/CommonBeans.java
+++ common/src/main/java/pl/training/cloud/common/config/CommonBeans.java
@@ -1,5 +1,6 @@
 package pl.training.cloud.common.config;
 
+import feign.RequestInterceptor;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.training.cloud.common.feign.OAuth2FeignRequestInterceptor;
 import pl.training.cloud.common.model.Mapper;
 
 @EnableCircuitBreaker
@@ -19,6 +21,11 @@ public class CommonBeans {
     @Bean
     public Mapper mapper(MessageSource messageSource) {
         return new Mapper(messageSource);
+    }
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor() {
+        return new OAuth2FeignRequestInterceptor();
     }
 
 }
