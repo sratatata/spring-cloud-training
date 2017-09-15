5ee802a9776de24fa4cea55c32db5efa482f673b
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-15 14:23:40 +0200

    Docker

--- users/docker/run.sh
+++ users/docker/run.sh
@@ -5,4 +5,4 @@ while ! `nc -z discovery_server $DEFAULT_PORT`;
     do sleep $PING_TIMEOUT;echo "Waiting for discovery server to start...";done
 while ! `nc -z users_database 5432`;
     do sleep $PING_TIMEOUT;echo "Waiting for users database to start...";done
-java -jar -Dspring.profiles.active=$DEFAULT_PROFILE -Dspring.cloud.config.uri=$CONFIGURATION_SERVER_URI /usr/local/service/users.jar
+java -jar -Dcloud.stream.kafka.binder.zkNodes=$KAFKA_SERVER -Dcloud.stream.kafka.binder.brokers=$KAFKA_SERVER -Dspring.profiles.active=$DEFAULT_PROFILE -Dspring.cloud.config.uri=$CONFIGURATION_SERVER_URI /usr/local/service/users.jar
