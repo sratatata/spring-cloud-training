51c5fdb5d7c48807abe4476bca486ae7da128734
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-15 11:28:55 +0200

    Docker

--- departments/docker/run.sh
+++ departments/docker/run.sh
@@ -5,4 +5,4 @@ while ! `nc -z discovery_server $DEFAULT_PORT`;
     do sleep $PING_TIMEOUT;echo "Waiting for discovery server to start...";done
 while ! `nc -z departments_database 5432`;
     do sleep $PING_TIMEOUT;echo "Waiting for departments database to start...";done
-java -jar -Dspring.cloud.config.uri=$CONFIGURATION_SERVER_URI /usr/local/service/departments.jar
+java -jar -Dspring.profiles.active=$DEFAULT_PROFIL -Dspring.cloud.config.uri=$CONFIGURATION_SERVER_URI /usr/local/service/departments.jar
