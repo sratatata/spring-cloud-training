001dda92b3ff23de761d9a1921b0544ae8fbf067
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-15 15:37:56 +0200

    Docker

--- configuration/docker/run.sh
+++ configuration/docker/run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java -jar /usr/local/service/configuration.jar
+while ! `nc -z kafka_server 9092`;
+    do sleep $PING_TIMEOUT;echo "Waiting for kafka server to start...";done
+java -jar -Dspring.profiles.active=$DEFAULT_PROFILE /usr/local/service/configuration.jar
--- users/docker/run.sh
+++ users/docker/run.sh
@@ -5,4 +5,6 @@ while ! `nc -z discovery_server $DEFAULT_PORT`;
     do sleep $PING_TIMEOUT;echo "Waiting for discovery server to start...";done
 while ! `nc -z users_database 5432`;
     do sleep $PING_TIMEOUT;echo "Waiting for users database to start...";done
+while ! `nc -z kafka_server 9092`;
+    do sleep $PING_TIMEOUT;echo "Waiting for kafka server to start...";done
 java -jar -Dspring.profiles.active=$DEFAULT_PROFILE -Dspring.cloud.config.uri=$CONFIGURATION_SERVER_URI /usr/local/service/users.jar
