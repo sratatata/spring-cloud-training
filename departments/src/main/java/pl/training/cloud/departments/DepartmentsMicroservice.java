baf0bb3b3feecdd3a81647af82e50f494f108887
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-15 13:31:42 +0200

    Docker

--- departments/src/main/java/pl/training/cloud/departments/DepartmentsMicroservice.java
+++ departments/src/main/java/pl/training/cloud/departments/DepartmentsMicroservice.java
@@ -2,12 +2,22 @@ package pl.training.cloud.departments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import pl.training.cloud.departments.model.Message;
 
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class DepartmentsMicroservice {
 
     public static void main(String[] args) {
         SpringApplication.run(DepartmentsMicroservice.class);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void onMessage(Message message) {
+        System.out.println("New message: " + message);
     }
 
 }
--- departments/src/main/java/pl/training/cloud/departments/config/Beans.java
+++ departments/src/main/java/pl/training/cloud/departments/config/Beans.java
@@ -1,16 +1,11 @@
 package pl.training.cloud.departments.config;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import pl.training.cloud.departments.model.Message;
 
-@EnableBinding(Sink.class)
 @ComponentScan(basePackages = "pl.training.cloud.common")
 @Configuration
 public class Beans {
@@ -21,11 +16,6 @@ public class Beans {
         messageSource.setDefaultEncoding("utf-8");
         messageSource.setBasename("exceptions");
         return messageSource;
-    }
-
-    @StreamListener(Sink.INPUT)
-    public void onMessage(Message message) {
-        System.out.println("New message: " + message);
     }
 
 }
