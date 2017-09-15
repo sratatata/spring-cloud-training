d4da45ac552e42271888cacd7d13a69d9a51c4b7
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-15 09:32:41 +0200

    Docker

--- gateway/docker/run.sh
+++ gateway/docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-while ! `nc -z configuration_server $CONFIGURATION_SERVER_PORT`;
+while ! `nc -z configuration_server $DEFAULT_PORT`;
     do sleep $PING_TIMEOUT;echo "Waiting for configuration server to start...";done
-while ! `nc -z discovery_server $DISCOVERY_SERVER_PORT`;
+while ! `nc -z discovery_server $DEFAULT_PORT`;
     do sleep $PING_TIMEOUT;echo "Waiting for discovery server to start...";done
 java -jar -Dspring.cloud.config.uri=$CONFIGURATION_SERVER_URI /usr/local/service/gateway.jar
